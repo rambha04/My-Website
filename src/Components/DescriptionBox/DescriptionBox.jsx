bb7e5805dde8ed01b440d67b0b56386999420f60
Rambha <rambhaverma944@gmail.com>
2025-04-03 20:14:26 +0530

    Successful build and ready for deployment

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './DescriptionBox.css';
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+          <div className="description-navigator">
+             <div className="description-nav-box"> Description </div>
+             <div className="description-nav-box fade"> Reviews(122) </div>
+          </div>
+          <div className="descriptionbox-description">
+            
+          </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
